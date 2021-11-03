0de3331b9d97eadc7282877146cf2e39c27d8392
Rachelle <rachelle.yeo@outlook.com>
2021-11-03 12:56:40 +0800

    Set datafields and data state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import axios from "axios";
 // API returns
 // "records": [{"_id":1,"Reported Date":"01/07/2019","Suburb - Incident":"ADELAIDE","Postcode - Incident":"5000","Offence Level 1 Description":"OFFENCES AGAINST PROPERTY","Offence Level 2 Description":"FRAUD DECEPTION AND RELATED OFFENCES","Offence Level 3 Description":"Obtain benefit by deception","Offence count":"1"}]
 
+// fields data
+// 0: {id: '_id', type: 'int'}
+// 1: {id: 'Reported Date', type: 'text'}
+// 2: {id: 'Suburb - Incident', type: 'text'}
+// 3: {id: 'Postcode - Incident', type: 'text'}
+// 4: {id: 'Offence Level 1 Description', type: 'text'}
+// 5: {id: 'Offence Level 2 Description', type: 'text'}
+// 6: {id: 'Offence Level 3 Description', type: 'text'}
+// 7: {id: 'Offence count', type: 'text'}
+
 function App() {
   const [dataFieldsId, setDataFieldsId] = useState([]);
   const [data, setData] = useState([]);
@@ -18,8 +28,10 @@ function App() {
       const res = await axios
         .get(apiUrl)
         .then((response) => {
-          console.log(response.data.result.fields);
-          setDataFieldsId(response.data.result.fields);
+          const dataFields = response.data.result.fields;
+          const data = response.data.result.records;
+          setDataFieldsId(dataFields);
+          setData(data);
         })
         .catch((error) => console.log(error));
     };
