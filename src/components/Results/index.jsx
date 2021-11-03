815462a6629501a0b2a2307b37d02b48af2691dc
Rachelle <rachelle.yeo@outlook.com>
2021-11-03 21:11:55 +0800

    Managed to print all data out :)
    Yay!

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,22 +1,57 @@
 import styled from "styled-components";
 
 function Results({ dataFieldsId, records }) {
-  const cities = [];
+  const storedData = [];
 
   dataFieldsId.map((item) => {
     if (item.id === "Suburb - Incident") {
       records.map((subitem) => {
         const city = subitem["Suburb - Incident"];
-        if (cities.includes(city)) {
-          return;
-        }
-        cities.push(city);
+        const date = subitem["Reported Date"];
+        const postcode = subitem["Postcode - Incident"];
+        const offenceCount = subitem["Offence count"];
+        const offenceL1 = subitem["Offence Level 1 Description"];
+        const offenceL2 = subitem["Offence Level 2 Description"];
+        const offenceL3 = subitem["Offence Level 3 Description"];
+        storedData.push({
+          city: city,
+          date: date,
+          postcode: postcode,
+          offenceCount: offenceCount,
+          offenceL1: offenceL1,
+          offenceL2: offenceL2,
+          offenceL3: offenceL3,
+        });
       });
     }
   });
 
-  const mappedCities = cities.map((item) => {
-    return <ButtonWrapper>{item}</ButtonWrapper>;
+  console.log(storedData);
+
+  const mappedCities = storedData.map((item) => {
+    const {
+      city,
+      date,
+      postcode,
+      offenceCount,
+      offenceL1,
+      offenceL2,
+      offenceL3,
+    } = item;
+    return (
+      <AccordionWrapper key={`${item.city}-${item.date}-${item.offenceL3}`}>
+        <div className="accordion-title">
+          {city} {postcode}
+        </div>
+        <div className="accordion-content">
+          <div className="accordion-item">
+            L1: {offenceL1}, {offenceL2} <br />
+            {offenceL3} <br />
+            {date} {offenceCount}
+          </div>
+        </div>
+      </AccordionWrapper>
+    );
   });
 
   return <ResultsWrapper>{mappedCities}</ResultsWrapper>;
@@ -29,11 +64,14 @@ const ResultsWrapper = styled.div`
   align-items: middle;
 `;
 
-const ButtonWrapper = styled.button`
-  background: #e774f7;
-  padding: 8px 5px;
+const AccordionWrapper = styled.div`
   margin-bottom: 15px;
-  outline: none;
-  border: none;
-  width: 100%;
+  background: beige;
+  .accordion-title {
+    font-size: 25px;
+    background: turquoise;
+  }
+  .accordion-content {
+    padding: 20px;
+  }
 `;
